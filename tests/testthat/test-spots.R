5e9c544ebbadac073ed57107e3f6a0cbd99c4888
sydney-grant <sydney.grant@roswellpark.org>
2023-08-07 10:35:13 -0400

    error fix

--- tests/testthat/test-spots.R
+++ tests/testthat/test-spots.R
@@ -1,21 +1,21 @@
 data("example_mutations")
 data("example_regions")
-spots <- sig.spots(data = example_mutations, regions = example_regions, pvalue = 0.05, threshold = 0.2, 
+spots <- sig.spots(data = example_mutations, regions = example_regions, pvalue = 0.05, threshold = 0.2,
                    include_genes = TRUE, rank = TRUE)
 
 
-test_that("forward hotspot panel is valid", {
+test_that("spots are valid", {
   hotspots <- subset(regions, type == "Hotspot")
   nonhotspots <- subset(regions, type == "Non-hotspot")
   h_count <- hotspots$Count / length(unique(data$Sample))
   nh_count <- nonhotspots$Count / length(unique(data$Sample))
-  
+
   suppressWarnings({ks <- ks.test(h_count, nh_count, alternative = "greater")})
   pv <- ks$p.value
   d <- ks$statistic
-  
+
   if (pv < pvalue & d > threshold){check = "yes"}
   else{check = "no"}
-  
+
   expect_equal(check, "yes")
 })
