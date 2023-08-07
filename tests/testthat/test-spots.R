c526c8e964a751b8fda18d19e66653a43fdf1300
sydney-grant <sydney.grant@roswellpark.org>
2023-08-07 12:22:29 -0400

    testthat fix

--- tests/testthat/test-spots.R
+++ tests/testthat/test-spots.R
@@ -10,10 +10,28 @@ spots <- sig.spots(data = example_mutations, regions = example_regions, pvalue =
 test_that("spots are valid", {
   hotspots <- subset(spots[[1]], type == "Hotspot")
   nonhotspots <- subset(spots[[1]], type == "Non-hotspot")
-  h_count <- hotspots$Count / length(unique(example_mutations$Sample))
-  nh_count <- nonhotspots$Count / length(unique(example_mutations$Sample))
+  count_h <- function(x){
+    count <- 0
+    sub <- subset(example_mutations, Sample == x)
+    for (i in seq_len(nrow(hotspots))){
+      sub2 <- subset(sub, Chromosome == hotspots$Chromosome[[i]] & Position %in% hotspots$Lowerbound[[i]]:hotspots$Upperbound[[i]])
+      count <- count + nrow(sub2)
+    }
+    return(count/nrow(hotspots))
+  }
+  count_nh <- function(x){
+    count <- 0
+    sub <- subset(example_mutations, Sample == x)
+    for (i in seq_len(nrow(nonhotspots))){
+      sub2 <- subset(sub, Chromosome == nonhotspots$Chromosome[[i]] & Position %in% nonhotspots$Lowerbound[[i]]:nonhotspots$Upperbound[[i]])
+      count <- count + nrow(sub2)
+    }
+    return(count/nrow(nonhotspots))
+  }
+  h_count <- lapply(unique(example_mutations$Sample), count_h)
+  nh_count <- lapply(unique(example_mutations$Sample), count_nh)
 
-  suppressWarnings({ks <- ks.test(h_count, nh_count, alternative = "greater")})
+  suppressWarnings({ks <- ks.test(unlist(h_count), unlist(nh_count), alternative = "greater")})
   pv <- ks$p.value
   d <- ks$statistic
 
